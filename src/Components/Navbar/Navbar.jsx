c1a171d340d982f274ee2fb67ed2fd7263d3aebc
irem sultan örün <iremsultanorun2@gmail.com>
2026-02-09 19:15:47 +0300

    eksik class isimleri tamamlandı. responsive düzenlemeler yapıldı

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
 import './Navbar.css'
 import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                {/* Redirects are created using Link and HashLink */}
+        <nav className='nav'>
+            <ul className='nav-items-container'>
                 <HashLink to="/#home-section" smooth> <li className='nav-item about'>About</li></HashLink>
                 <HashLink to="/#collections-section" smooth> <li className='nav-item '>Collections</li></HashLink>
                 <HashLink to="/#roadmap-section" smooth> <li className='nav-item roadmap'>Roadmap</li></HashLink>
-
                 <Link to="/placeholder-page"><li className='nav-item tutorial'>
                     Tutorial
                 </li>
